e770618523c36ecf5afdf5c823ab14ae237ee15d
Zaven <matthew.fullstackgis@gmail.com>
2019-07-12 14:19:44 -0700

    Set up cron job for assinging clusters to new trees

---
+++ server/cron/assign-new-trees-to-clusters.js
@@ -0,0 +1,50 @@
+
+var pg = require('pg');
+const { Pool, Client } = require('pg');
+var config = require('./../config/config');
+const pool = new Pool({
+  connectionString: config.connectionString
+});
+
+
+(async () => {
+
+  const start = Date.now();
+
+  const client = await pool.connect();
+  const insert = {
+    text: `INSERT INTO tree_region
+      (tree_id, zoom_level, region_id)
+      SELECT DISTINCT ON (trees.id, zoom_level) trees.id AS tree_id, zoom_level, region.id
+      FROM trees
+      JOIN region
+      ON ST_Contains( region.geom, trees.estimated_geometric_location)
+      JOIN region_zoom
+      ON region_zoom.region_id = region.id
+      WHERE trees.active = true
+      AND trees.cluster_regions_assigned = false
+      ORDER BY trees.id, zoom_level, region_zoom.priority DESC`
+  };
+  await client.query(insert);
+  await client.query('COMMIT');
+
+  const update = {
+    text: `UPDATE trees
+      SET cluster_regions_assigned = true
+      FROM tree_region
+      WHERE tree_region.tree_id = trees.id
+      AND cluster_regions_assigned = false`
+  };
+  await client.query(update);
+  await client.query('COMMIT');
+
+
+  pool.end();
+
+  const end = Date.now();
+  console.log(start);
+  console.log(end);
+  console.log('DONE');
+
+})().catch(e => console.error(e.stack))
+
---
+++ server/cron/assign-new-trees-to-clusters.sh
@@ -0,0 +1,14 @@
+REBUILD_CLUSTERS_PID_FILE=/var/run/assign-clusters.pid
+
+if [ -e $REBUILD_CLUSTERS_PID_FILE ]; then
+  echo "script is already running"
+  exit
+fi
+
+# Ensure PID file is removed on program exit.
+trap "rm -f -- '$REBUILD_CLUSTERS_PID_FILE'" EXIT
+
+# Create a file with current PID to indicate that process is running.
+echo $$ > "$REBUILD_CLUSTERS_PID_FILE"
+
+/usr/bin/node /root/treetracker-web-map/server/cron/assign-new-trees-to-clusters.js >>/root/treetracker-web-map/server/cron/log-file.txt 2>/root/treetracker-web-map/server/cron/log-file.txt
