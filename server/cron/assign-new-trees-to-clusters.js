3d6ac7b7187125fea2d20402f562c12e21b327f7
Zaven <zaven@greenstand.org>
2019-07-23 02:46:02 +0000

    Cron job for filtering deactivated trees from clusters

--- server/cron/assign-new-trees-to-clusters.js
+++ server/cron/assign-new-trees-to-clusters.js
@@ -12,6 +12,22 @@ const pool = new Pool({
   const start = Date.now();
 
   const client = await pool.connect();
+
+  const select = {
+    text: `SELECT count(id)
+    FROM trees
+    WHERE trees.active = true
+    AND trees.cluster_regions_assigned = false`
+  };
+  const rval = await client.query(select);
+  if(rval.rows[0].count == 0){
+    console.log("no new trees");
+    client.release();
+    pool.end();
+    return;
+  }
+
+  await client.query('BEGIN');
   const insert = {
     text: `INSERT INTO tree_region
       (tree_id, zoom_level, region_id)
@@ -29,6 +45,7 @@ const pool = new Pool({
   await client.query(insert);
   await client.query('COMMIT');
 
+  await client.query('BEGIN');
   const update = {
     text: `UPDATE trees
       SET cluster_regions_assigned = true
@@ -46,12 +63,49 @@ const pool = new Pool({
 
   await client.query('COMMIT');
 
+
+  await client.query('BEGIN');
+
+  sql = `SELECT 'cluster' AS type,
+    St_centroid(clustered_locations) centroid,
+    St_numgeometries(clustered_locations) count
+    FROM
+    (
+      SELECT Unnest(St_clusterwithin(estimated_geometric_location, 0.005)) clustered_locations
+      FROM   trees
+      WHERE  active = true
+    ) clusters`;
+  query = {
+    text: sql,
+  };
+  console.log(query);
+
+  const {rows} = await client.query(query);
+  zoomLevel = 14;
+  await client.query('DELETE FROM clusters WHERE zoom_level = ' + zoomLevel);
+  for (let row in rows) {
+    query = {
+      text: 'INSERT INTO clusters (count, zoom_level, location) values ($1, $2, $3 ) RETURNING *',
+      values: [rows[row]['count'], zoomLevel, rows[row]['centroid']]
+    };
+    await client.query(query);
+  }
+  await client.query('COMMIT');
+  console.log('generated clusters at zoom level ' + zoomLevel);
+
   client.release();
   pool.end();
 
   const end = Date.now();
-  console.log(start);
-  console.log(end);
+  console.log( "Start: " + start);
+  console.log( "End: " + end);
+  console.log( "Elapsed: " + end - start);
   console.log('DONE');
 
-})().catch(e => console.error(e.stack))
+})().catch( async function(e){
+    console.error(e.stack);
+    await client.query('ROLLBACK');
+    client.release();
+    pool.end();
+  }
+);
