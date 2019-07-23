c492c3d295f70556927d27402a2bdf780202996f
Zaven <zaven@greenstand.org>
2019-07-23 20:24:49 +0000

    Fix cron job script to reference right js script

--- server/cron/update-clusters-for-deactivated-trees.sh
+++ server/cron/update-clusters-for-deactivated-trees.sh
@@ -11,4 +11,4 @@ trap "rm -f -- '$REBUILD_CLUSTERS_PID_FILE'" EXIT
 # Create a file with current PID to indicate that process is running.
 echo $$ > "$REBUILD_CLUSTERS_PID_FILE"
 
-/usr/bin/node /root/treetracker-web-map/server/cron/rebuild-detailed-clusters-only-global.js >>/root/treetracker-web-map/server/cron/log-file.txt 2>/root/treetracker-web-map/server/cron/log-file.txt
+/usr/bin/node /root/treetracker-web-map/server/cron/update-clusters-for-deactivated-trees.js >>/root/treetracker-web-map/server/cron/log-file.txt 2>/root/treetracker-web-map/server/cron/log-file.txt
