37ae77300eca0e8a7d9de40b3f442c2d6ba98745
Zaven <matthew.fullstackgis@gmail.com>
2019-07-22 18:10:54 -0700

    Use fat arrow syntax

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 app.set('view engine', 'html');
 
-var allowCrossDomain = function(req, res, next) {
+const allowCrossDomain = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
