0eb6023d3749a59e71abf09e343d4e73934975df
Zaven <matthew.fullstackgis@gmail.com>
2020-01-09 16:33:39 -0800

    Remove old config stuff

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -1,14 +1,2 @@
 exports.connectionString = "postgres://user:password@host/treetrackerdb";
-exports.optimizedBounds = '57.0,9.0,17.0,-16.0';
-
-exports.smtpSettings = {
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: "",
-        pass: ""
-    }
-};
 
