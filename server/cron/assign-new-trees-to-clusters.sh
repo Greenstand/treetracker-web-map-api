dbc0dfe53d9490d2b08a62249e5dce91ec00db39
Zaven <zaven@greenstand.org>
2020-02-16 00:00:10 +0000

    Fix logging for assign new trees script

--- server/cron/assign-new-trees-to-clusters.sh
+++ server/cron/assign-new-trees-to-clusters.sh
@@ -11,4 +11,4 @@ trap "rm -f -- '$REBUILD_CLUSTERS_PID_FILE'" EXIT
 # Create a file with current PID to indicate that process is running.
 echo $$ > "$REBUILD_CLUSTERS_PID_FILE"
 
-/usr/bin/node /root/treetracker-web-map/server/cron/assign-new-trees-to-clusters.js >>/root/treetracker-web-map/server/cron/log-file.txt 2>/root/treetracker-web-map/server/cron/log-file.txt
+/usr/bin/node /root/treetracker-web-map/server/cron/assign-new-trees-to-clusters.js >>/root/treetracker-web-map/server/cron/assign-clusters-log-file.txt 2>>/root/treetracker-web-map/server/cron/assign-clusters-log-file.txt
