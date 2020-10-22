ceb247a8a2539866f312b8397cb41c3bfeba0410
deanchen <antlet2004@gmail.com>
2020-10-22 11:39:05 +0800

    Added: query with organization for other two cases

--- server/__tests__/integration.test.js
+++ server/__tests__/integration.test.js
@@ -8,6 +8,17 @@ describe("Integration tests", () => {
     const res = await request(app)
       .get("/trees?clusterRadius=8&zoom_level=2&map_name=freetown");
     expect(res.statusCode).toBe(200);
-
   });
+
+  it("http://localhost:3000/trees?clusterRadius=0.02&zoom_level=12&bounds=0.27413497359721345,0.09675638212298909,-0.3191267451527627,-0.07998612103164923&map_name=freetown", async () => {
+    const res = await request(app)
+      .get("/trees?clusterRadius=0.02&zoom_level=12&bounds=0.27413497359721345,0.09675638212298909,-0.3191267451527627,-0.07998612103164923&map_name=freetown");
+    expect(res.statusCode).toBe(200);
+  });
+
+  it.only("http://localhost:3000/trees?clusterRadius=0.003&zoom_level=16&bounds=0.018839836120605472,0.005394458765216459,-0.018239021301269535,-0.0056519508298092415&map_name=freetown", async () => {
+    const res = await request(app)
+      .get("/trees?clusterRadius=0.003&zoom_level=16&bounds=0.018839836120605472,0.005394458765216459,-0.018239021301269535,-0.0056519508298092415&map_name=freetown");
+    expect(res.statusCode).toBe(200);
+  }, 10000);
 });
