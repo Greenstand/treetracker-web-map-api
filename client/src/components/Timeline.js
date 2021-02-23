2d2db5f01651724368700964b4bb07bc83149dd5
deanchen <antlet2004@gmail.com>
2021-02-23 11:13:47 +0800

    feat: appearnce of timeline component

--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -2,10 +2,65 @@ import log from "loglevel";
 import React from "react";
 import AccessTime from "@material-ui/icons/AccessTime";
 import Grid from "@material-ui/core/Grid";
-import {makeStyles} from "@material-ui/core/styles";
+import Box from "@material-ui/core/Grid";
+import {withStyles, makeStyles} from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import Slider from '@material-ui/core/Slider';
 import moment from "moment";
+import PropTypes from 'prop-types';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const TimelineSlider = withStyles({
+  root: {
+    color: '#85c232',
+    height: 8,
+    "& .MuiSlider-markLabel" :{
+      color: "white",
+    },
+    "& .MuiSlider-markLabelActive": {
+      color: "white",
+    },
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    marginTop: -8,
+    marginLeft: -12,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
+ValueLabelComponent.propTypes = {
+  children: PropTypes.element.isRequired,
+  open: PropTypes.bool.isRequired,
+  value: PropTypes.number.isRequired,
+};
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,6 +75,8 @@ const useStyles = makeStyles(theme => ({
   },
   box2: {
 //    width: theme.spacing(10),
+    padding: theme.spacing(2),
+    marginTop: -21,
   },
   box3: {
     minWidth: theme.spacing(70),
@@ -27,6 +84,13 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const dayRange = Math.round(moment.duration(moment().diff(moment("2015-01-01"))).as("d"));
+
+const marks = ['2015', '2016', '2017', '2018', '2019', '2020', '2021'].map(e => {
+  return ({
+    label: e,
+    value: Math.round(moment.duration(moment(`${e}-01-01`).diff(moment(`2015-01-01`))).as("d")),
+  });
+});
 
 function valuetext(value) {
   return moment('2015-01-01').add(value, "days").format("YYYY-MM-DD");
@@ -82,16 +146,18 @@ function Timeline(props){
           </Grid>
           <Grid item  className={classes.box3} >
             {slide &&
-              <Slider
+              <TimelineSlider
               min={0}
               max={dayRange}
               value={value}
               onChange={handleChange}
               onChangeCommitted={handleChangeCommitted}
-              valueLabelDisplay="auto"
               aria-labelledby="range-slider"
               getAriaValueText={valuetext}
               valueLabelFormat={valuetext}
+              marks={marks}
+              valueLabelDisplay="on"
+              ValueLabelComponent={ValueLabelComponent}
             />
             }
           </Grid>
