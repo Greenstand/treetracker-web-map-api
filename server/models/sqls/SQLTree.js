5c2aac1e1f101991594e49a6f846609a926b2130
Zaven <matthew.fullstackgis@gmail.com>
2021-03-04 16:48:45 -0800

    fix: keep fixing SQL

--- server/models/sqls/SQLTree.js
+++ server/models/sqls/SQLTree.js
@@ -18,15 +18,15 @@ class SQLTree{
           planter.first_name AS first_name, planter.last_name as last_name,
           planter.image_url AS user_image_url,
           token.id::text AS token_uuid,
-          wallet.wallet as wallet
+          wallet.name as wallet
         FROM
           trees
         INNER JOIN planter
         ON planter.id = trees.planter_id
         LEFT JOIN tree_species ON
           trees.species_id = tree_species.id
-        LEFT JOIN wallet.token token ON wallet.token.capture_id::text = trees.uuid
-        LEFT JOIN wallet.wallet wallet ON wallet.id = wallet.token.wallet_id 
+        LEFT JOIN wallet.token token ON token.capture_id::text = trees.uuid
+        LEFT JOIN wallet.wallet wallet ON wallet.id = token.wallet_id 
         WHERE
           trees.id = $1 AND trees.active = true
       `,
