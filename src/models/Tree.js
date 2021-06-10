ee16e63eb9fa8a2618b7fc989d0d2fa0db3f3f4b
deanchen <antlet2004@gmail.com>
2021-06-10 11:55:00 +0800

    feat: support api: /tree?tree_name=xxx

--- src/models/Tree.js
+++ src/models/Tree.js
@@ -57,6 +57,31 @@ class Tree{
     return treeObject;
   }
 
+  async getTreeByName(treeName){
+    const sql = new SQLTree();
+    sql.setTreeName(treeName);
+    const query = await sql.getQuery();
+    const result = await this.pool.query(query);
+    if(result.rows.length === 0){
+      throw new Error("can not find tree", treeId);
+    }
+    const treeObject = result.rows[0];
+    //attribute
+    {
+      const query = {
+        text: "select * from tree_attributes where tree_id = $1",
+        values: [treeObject.id],
+      };
+      const attributes = await this.pool.query(query);
+      const attributeJson = {};
+      for(const r of attributes.rows){
+        attributeJson[r.key] = r.value;
+      }
+      treeObject.attributes = attributeJson;
+    }
+    return treeObject;
+  }
+
 }
 
 
--- src/models/Tree.test.js
+++ src/models/Tree.test.js
@@ -1,5 +1,6 @@
 const Tree = require("./Tree");
 const {Pool} = require("pg");
+const {SQLTree} = require("./sqls/SQLTree");
 jest.mock("pg");
 
 describe("Tree", () => {
@@ -34,6 +35,39 @@ describe("Tree", () => {
     expect(treeDetail.attributes).toMatchObject({
       key1: "value1",
       key2: "value2",
+    });
+  });
+
+  it("getTreeByName", async () => {
+    query
+      .mockResolvedValueOnce({
+        rows: [{
+          id: 1,
+          domain_specific_data: {},
+        }],
+      })
+      .mockResolvedValueOnce({
+        rows: [{
+            key: "key1",
+            value: "value1",
+          },{
+            key: "key2",
+            value: "value2",
+          }],
+      });
+    const tree = new Tree();
+    const treeDetail = await tree.getTreeByName("a_b_c");
+    expect(treeDetail.id).toBe(1);
+    expect(treeDetail.domain_specific_data).toMatchObject({});
+    expect(treeDetail.attributes).toMatchObject({
+      key1: "value1",
+      key2: "value2",
+    });
+    expect(query).toHaveBeenCalledTimes(2);
+    expect(query.mock.calls[0][0].text).toMatch(/trees.name = 'a_b_c'/);
+    expect(query.mock.calls[1][0]).toMatchObject({
+      text: "select * from tree_attributes where tree_id = $1",
+      values: [1],
     });
   });
 
